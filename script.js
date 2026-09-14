b2e134a30f4b4539309b6651df80eb586aa93d63
MarvelcoW <marvelcowongso@gmail.com>
2026-09-14 08:04:57 +0700

    Front End Assignment 2

---
+++ script.js
@@ -0,0 +1,8 @@
+function tampilkanData() {
+    document.getElementById("data").innerHTML =
+        "Nama: Marvelco Wongso<br>NIM: Belum Dapet";
+}
+
+function hapusData() {
+    document.getElementById("data").innerHTML = "Nama dan NIM Berhasil di Hapus";
+}
